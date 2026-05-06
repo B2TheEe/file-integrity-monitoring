c66e88c069b8ce7d5df81af4a8a6fcc703532ef2
Bente Schopman <schopmanbenteb2theee@gmail.com>
2026-05-06 20:47:37 +0200

    Base

---
+++ fim.py
@@ -0,0 +1,31 @@
+class FileHasher():
+    def __init__(self):
+        pass
+
+class BaselineManager():
+    def __init__(self):
+        pass
+
+
+class FileScanner():
+    def __init__(self):
+        pass
+
+class IntegrityChecker():
+    def __init__(self):
+        pass
+
+
+class AlertManager():
+    def __init__(self):
+        pass
+
+class Logger():
+    def __init__(self):
+        pass
+
+class Monitor():
+    def __init__(self):
+        pass
+    def run(self):
+        pass
